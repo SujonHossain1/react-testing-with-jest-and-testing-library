460be32f70aac1fc1a1b2fa107f7476e85d12347
Sujon Hossain <sujon.hossain758@gmail.com>
2021-11-16 16:38:21 +0600

    learn describe, user event, aria-lebel selector

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,7 +1,39 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import Counter from './Counter';
 
 describe('Counter', () => {
+    describe('initialized with defaultCount = 10, description = "WWW"', () => {
+        beforeEach(() => {
+            render(<Counter description="count" defaultCount={10} />);
+        });
+
+        it('the counter = 10', () => {
+            expect(screen.getByText('Current Count: 10')).toBeInTheDocument();
+        });
+
+        describe('when the incrementor changes to 5 and plus button clicked', () => {
+            beforeEach(() => {
+                user.type(screen.getByLabelText(/Incrementor/), '{selectall}5')
+                user.click(screen.getByRole('button', { name: 'increment' }));
+            });
+            it('then counter = 15', () => {
+                expect(screen.getByText('Current Count: 15')).toBeInTheDocument();
+            });
+        })
+
+        describe('when the decrement changes to 5 and minus button clicked', () => {
+            beforeEach(() => {
+                user.type(screen.getByLabelText(/Incrementor/), '{selectall}5')
+                user.click(screen.getByRole('button', { name: 'decrement' }));
+            });
+            it('then counter = 5', () => {
+                expect(screen.getByText('Current Count: 5')).toBeInTheDocument();
+            });
+        })
+
+    });
+
     describe('initialized with defaultCount = 0, description = "count"', () => {
         beforeEach(() => {
             render(<Counter description="count" defaultCount={0} />);
@@ -11,16 +43,26 @@ describe('Counter', () => {
             expect(screen.getByText('Current Count: 0')).toBeInTheDocument();
         });
 
-        it('clicked plus button then counter = 1', () => {
-            fireEvent.click(screen.getByRole('button', { name: '+' }));
-            expect(screen.getByText('Current Count: 1')).toBeInTheDocument();
+        describe('when plus button is clicked', () => {
+            beforeEach(() => {
+                fireEvent.click(screen.getByRole('button', { name: 'increment' }));
+            });
+            it('render counter = 1', () => {
+                expect(
+                    screen.getByText('Current Count: 1')
+                ).toBeInTheDocument();
+            });
         });
 
-        it('clicked minus button then counter = -1', () => {
-            const minusButton = screen.getByText('-');
-            minusButton.click();
-
-            expect(screen.getByText('Current Count: -1')).toBeInTheDocument();
+        describe('when minus button is clicked', () => {
+            beforeEach(() => {
+                fireEvent.click(screen.getByRole('button', { name: 'decrement' }));
+            });
+            it('render counter = -1', () => {
+                expect(
+                    screen.getByText('Current Count: -1')
+                ).toBeInTheDocument();
+            });
         });
     });
 });
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,14 +7,20 @@ interface IProps {
 
 const Counter = ({ description, defaultCount }: IProps) => {
     const [count, setCount] = useState(defaultCount);
+    const [incrementor, setIncrementor] = useState(1);
+
     return (
         <div>
             <h2>
                 DESC: {description} - DC: {defaultCount}
             </h2>
-            <button onClick={() => setCount(count - 1)}> - </button>
+            <label>
+                Incrementor:
+                <input value={incrementor} onChange={(event) => setIncrementor(parseInt(event.target.value) || 0)} type="number" />
+            </label>
+            <button aria-label="decrement" onClick={() => setCount(count - incrementor)}> - </button>
             Current Count: {count}
-            <button onClick={() => setCount(count + 1)}> + </button>
+            <button aria-label="increment" onClick={() => setCount(count + incrementor)}> + </button>
         </div>
     );
 };
